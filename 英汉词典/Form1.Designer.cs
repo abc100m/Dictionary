1bb2cc058d84fddf9f3aaea682551488b7d27ac6
HuoDa <huoda2008@163.com>
2013-12-27 21:54:57 +0800

    commit first

---
+++ 英汉词典/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace 英汉词典
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnQuery = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.cbSelect = new System.Windows.Forms.ComboBox();
+            this.lbResult = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.ForeColor = System.Drawing.Color.Gray;
+            this.txtInput.HideSelection = false;
+            this.txtInput.Location = new System.Drawing.Point(187, 35);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(147, 21);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.Text = "“回车”查询";
+            this.txtInput.MouseClick += new System.Windows.Forms.MouseEventHandler(this.txtInput_MouseClick);
+            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            // 
+            // btnQuery
+            // 
+            this.btnQuery.Location = new System.Drawing.Point(379, 33);
+            this.btnQuery.Name = "btnQuery";
+            this.btnQuery.Size = new System.Drawing.Size(75, 23);
+            this.btnQuery.TabIndex = 1;
+            this.btnQuery.Text = "查询";
+            this.btnQuery.UseVisualStyleBackColor = true;
+            this.btnQuery.Click += new System.EventHandler(this.btnQuery_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(187, 108);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtResult.Size = new System.Drawing.Size(147, 160);
+            this.txtResult.TabIndex = 2;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // cbSelect
+            // 
+            this.cbSelect.FormattingEnabled = true;
+            this.cbSelect.Location = new System.Drawing.Point(12, 36);
+            this.cbSelect.Name = "cbSelect";
+            this.cbSelect.Size = new System.Drawing.Size(121, 20);
+            this.cbSelect.TabIndex = 4;
+            this.cbSelect.SelectedIndexChanged += new System.EventHandler(this.cbSelect_SelectedIndexChanged);
+            // 
+            // lbResult
+            // 
+            this.lbResult.FormattingEnabled = true;
+            this.lbResult.ItemHeight = 12;
+            this.lbResult.Location = new System.Drawing.Point(13, 108);
+            this.lbResult.Name = "lbResult";
+            this.lbResult.ScrollAlwaysVisible = true;
+            this.lbResult.Size = new System.Drawing.Size(147, 160);
+            this.lbResult.TabIndex = 5;
+            this.lbResult.SelectedIndexChanged += new System.EventHandler(this.lbResult_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnQuery;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 303);
+            this.Controls.Add(this.lbResult);
+            this.Controls.Add(this.cbSelect);
+            this.Controls.Add(this.btnQuery);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnQuery;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ComboBox cbSelect;
+        private System.Windows.Forms.ListBox lbResult;
+    }
+}
+
---
+++ 英汉词典/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace 英汉词典
+{
+
+    public partial class Form1 : Form
+    {
+        string Input = "";
+        Dictionary<string, string> dic = new Dictionary<string, string>();
+        Dictionary<string, string> dicEnglish = new Dictionary<string, string>();
+        Dictionary<string, string> dicHuoxing = new Dictionary<string, string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            createEnDic("英汉词典TXT格式.txt");
+            createHxwDic("火星文.txt");
+            cbSelect.Items.Add("英汉词典");
+            cbSelect.Items.Add("火星文词典");
+            cbSelect.SelectedItem = cbSelect.Items[0];
+        }
+
+        private void btnQuery_Click(object sender, EventArgs e)
+        {
+            Input = txtInput.Text.Trim();
+            if (dic.ContainsKey(Input))
+            {
+                txtResult.Text = dic[Input];
+            }
+            else
+            {
+                txtResult.Text = "很遗憾，没有查询到";
+            }
+        }
+
+        private void createEnDic(string path)//生成英汉字典
+        {
+            string[] str = File.ReadAllLines(path, Encoding.Default);
+            for (int i = 0; i < str.Length; i++)
+            {
+                string[] str1 = str[i].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                if (str1.Length > 1)
+                {
+                    string s = "";
+                    for (int j = 1; j < str1.Length; j++)
+                    {
+                        s = s + str1[j] + "\r\n";
+                    }
+                    if (!dicEnglish.ContainsKey(str1[0]))
+                    {
+
+                        dicEnglish.Add(str1[0], s);
+                    }
+                    else
+                    {
+                        dicEnglish[str1[0]] += s;
+                    }
+
+                }
+            }
+        }
+
+        private void createHxwDic(string path)//生成火星文字典
+        {
+            string str = File.ReadAllText(path, Encoding.Default);
+            string[] s = str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!dicHuoxing.ContainsKey(s[i][0].ToString()))
+                {
+                    dicHuoxing.Add(s[i][0].ToString(), s[i].Substring(1, s[i].Length - 1));
+                }
+
+            }
+        }
+
+        private void cbSelect_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Input = "";
+            txtResult.Text = "";
+            lbResult.Items.Clear();
+            txtInput.Text = "";
+
+            if (cbSelect.SelectedItem.ToString() == "英汉词典")
+            {
+                dic = dicEnglish;
+            }
+            else
+            {
+                dic = dicHuoxing;
+            }
+            txtInput.Text = "\"回车\"查询";
+            txtInput.ForeColor = Color.Gray;
+
+        }
+
+        private void txtInput_MouseClick(object sender, MouseEventArgs e)
+        {
+            txtInput.Focus();
+            txtInput.SelectAll();
+        }
+
+        private void lbResult_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            txtResult.Text = dic[lbResult.SelectedItem.ToString()];
+        }
+
+        private void txtInput_TextChanged(object sender, EventArgs e)
+        {
+            txtInput.ForeColor = Color.Black;
+            if (txtInput.Text == "")
+            {
+                Input = "";
+                txtResult.Text = "";
+                lbResult.Items.Clear();
+                return;
+            }
+            lbResult.Items.Clear();
+            Input = txtInput.Text;
+            var v = from d in dic where d.Key.StartsWith(Input) select d;//进行模糊查询
+            foreach (var k in v)
+            {
+                lbResult.Items.Add(k.Key);
+            }
+        }
+    }
+}
